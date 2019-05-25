9312c56edd6c6d7d1f6bb6552a87d46023d2ef33
anneau <hkobori@gmx.com>
2019-05-25 18:03:14 +0900

    Add simple components

---
+++ miniita/src/components/organisms/TheNavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar is-primary">
+    <div class="container">
+      <div class="navbar-brand">
+        <nuxt-link
+          class="navbar-item brand-text"
+          to="/">
+          Miniita
+        </nuxt-link>
+        <div
+          class="navbar-burger burger"
+          data-target="navMenu">
+          <span/>
+          <span/>
+          <span/>
+        </div>
+      </div>
+      <div
+        id="navMenu"
+        class="navbar-menu">
+        <div class="navbar-end">
+          <nuxt-link
+            class="navbar-item"
+            to="/">
+            Home
+          </nuxt-link>
+          <nuxt-link
+            class="navbar-item"
+            to="/">
+            Settings
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<style scoped>
+nav.navbar {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ miniita/src/plugins/global-components.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import TheNavBar from '~/components/organisms/TheNavBar.vue';
+import AppTextArea from '~/components/atoms/AppTextArea.vue';
+import AppInputText from '~/components/atoms/AppInputText.vue';
+import AppButton from '~/components/atoms/AppButton.vue';
+
+Vue.component('TheNavBar', TheNavBar);
+Vue.component('AppTextArea', AppTextArea);
+Vue.component('AppInputText', AppInputText);
+Vue.component('AppButton', AppButton);
