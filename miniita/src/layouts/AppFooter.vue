3df9c9e337752f9b795b192fbcf4a10b2e5a3b6d
fussy113 <s1220080@gmail.com>
2019-05-25 17:15:34 +0900

    create layout files

---
+++ miniita/src/layouts/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="footer">
+  <div class="container">
+
+  </div>
+  footer
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    background-color: #55C502;
+    padding: 0 16px;
+  }
+</style>
--- miniita/src/pages/index.vue
+++ miniita/src/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="container">
+  <div class="index">
     <a href="#" class="button is-primary is-small">HELLO!</a>
   </div>
 </template>
 
 <script>
-
+export default {
+  layout: 'default',
+}
 </script>
 
 <style lang="scss" scoped>
-
+.index {
+  text-align: center;
+}
 </style>
